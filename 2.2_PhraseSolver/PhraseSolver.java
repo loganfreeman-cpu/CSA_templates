1ad5ebeb9804d4c72a08e82f26b320630850e01f
Mr Poling <227111198+mrPoling@users.noreply.github.com>
2025-11-06 22:53:20 -0800

    Initial state, from PLTW

---
+++ 2.2_PhraseSolver/PhraseSolver.java
@@ -0,0 +1,34 @@
+/*
+ * Activity 2.2.2
+ *
+ *  The PhraseSolver class the PhraseSolverGame
+ */
+import java.util.Scanner;
+  
+public class PhraseSolver
+{
+  /* your code here - attributes */
+
+  /* your code here - constructor(s) */ 
+
+  public void play()
+  {
+    boolean solved = false;
+    int currentPlayer = 1;
+
+    Scanner input = new Scanner(System.in);
+    
+    boolean correct = true;
+    while (!solved) 
+    {
+      
+      /* your code here - game logic */
+      
+      
+      /* your code here - determine how game ends */
+      solved = true; 
+    } 
+   
+  }
+  
+}
